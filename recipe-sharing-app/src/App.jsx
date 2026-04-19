16ee9a0242e3bd39e17f6f363460c6aa93db967b
Rob Tade <robtade4@gmail.com>
2026-04-19 12:44:58 +0300

    Add recipe app with Zustand state management

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
+
+function App() {
+  return (
+    <div>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
